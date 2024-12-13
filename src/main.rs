d0dddc9c77f085bacf94caddb46f65b56d9ba0c7
ThePolishTech_LP <noreply>
2024-12-13 16:33:46 +0100

    Added Comments to main.rs
    This is in sync with the ones in event_handler.rs. Not certain if the extra tabs would be considered a good idea, but with the "one tab back, encapsulating" it's kind of necessary for making it more readable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // --== ATTRIBUTES ==-- //
 #![allow(unused_braces)]
-#![feature(try_blocks)]
 
 // xxxxxxxxxxxxxxxxx //
 // --== CRATES == -- //
@@ -34,29 +33,33 @@ async fn main() {
 
     let bot_client: Result< serenity::Client, i32 > = 'main: {
 
-        print!("Opening Connection to Database...");
-        let sqlite_connection = match sqlite::open("kermmaw_db") {
-            Ok(conn) => {
-                println!("Ok");
-                conn
-            },
-            Err(why) => {
-                println!("Error: {why}");
-                break 'main Err( 1 );
-            }
-        };
+        // --== LOAD/CREATE DATABASE ==-- //
 
-        print!("Running Table Creation Script...");
-        match sqlite_connection.execute( sql_scripts::create_tables::SCRIPT ) {
-            Ok(_) => {
-                println!("Ok")
-            },
-            Err(why) => {
-                println!("Error: {why}");
-                break 'main Err( 1 );
-            }
-        }
+            print!("Opening Connection to Database...");
+            let sqlite_connection = match sqlite::open("kermmaw_db") {
+                Ok(conn) => {
+                    println!("Ok");
+                    conn
+                },
+                Err(why) => {
+                    println!("Error: {why}");
+                    break 'main Err( 1 );
+                }
+            };
 
+            print!("Running Table Creation Script...");
+            match sqlite_connection.execute( sql_scripts::create_tables::SCRIPT ) {
+                Ok(_) => {
+                    println!("Ok")
+                },
+                Err(why) => {
+                    println!("Error: {why}");
+                    break 'main Err( 1 );
+                }
+            }
+        // ==--
+
+        // Verify that there is a bot token in enviroment
         print!("Reading Bot Token From Enviromental Variable..."); 
         let bot_token = match env::var("BOT_TOKEN") {
             Ok(token) => {
@@ -69,24 +72,27 @@ async fn main() {
             }
         };
 
-        
-        let gateway_intents = GatewayIntents::GUILD_MESSAGES
-            | GatewayIntents::MESSAGE_CONTENT
-            | GatewayIntents::GUILD_MESSAGE_REACTIONS;
+        // --== SETUP CONNECTION TO GATEWAY ==-- //   
 
-        print!("Building Client...");
-        match Client::builder( bot_token, gateway_intents ).event_handler(event_handler::Handler).await {
-            Ok(client_builder) => {
-                println!("Ok");
-                break 'main Ok( client_builder )
-            },
-            Err(why) => {
-                println!("Error: {}", why);
-                break 'main Err( 1 );
+            let gateway_intents = GatewayIntents::GUILD_MESSAGES
+                | GatewayIntents::MESSAGE_CONTENT
+                | GatewayIntents::GUILD_MESSAGE_REACTIONS;
+
+            print!("Building Client...");
+            match Client::builder( bot_token, gateway_intents ).event_handler(event_handler::Handler).await {
+                Ok(client_builder) => {
+                    println!("Ok");
+                    break 'main Ok( client_builder )
+                },
+                Err(why) => {
+                    println!("Error: {}", why);
+                    break 'main Err( 1 );
+                }
             }
-        }
+        // ==--
     };
 
+    // Start the client
     print!("Starting Client...");
     match bot_client {
         Ok(mut client) => {
